55dd149c15b35be8d4faffcc36359131a7b1d89e
Nathaniel Schaaf <schana@users.noreply.github.com>
2025-04-25 13:29:52 -0500

    Add specific json serializers

--- Carim/Base/scripts/3_game/Carim/model/abc/disk_json.c
+++ Carim/Base/scripts/3_game/Carim/model/abc/disk_json.c
@@ -8,19 +8,9 @@ class CarimModelAbcDiskJson extends CarimModelAbcBase {
 
     override void Load() {
         if (FileExist(Path())) {
-            FileHandle handle = OpenFile(Path(), FileMode.READ);
-            if (handle == 0) {
-                CarimLogging.Error(this, "Cannot open file for reading " + Path());
-                return;
-            }
-
-            string fileContent;
-            ReadFile(handle, fileContent, 100000000);
-
-            CloseFile(handle);
-
-            if (!LoadData(fileContent)) {
-                CarimLogging.Warn(this, "Error loading " + Path());
+            string error;
+            if (!CarimModelAbcSerializers.LoadFile(this, error)) {
+                CarimLogging.Warn(this, "Error loading " + Path() + ", " + error);
             } else {
                 CarimLogging.Info(this, "Loaded " + Path());
             }
@@ -29,44 +19,12 @@ class CarimModelAbcDiskJson extends CarimModelAbcBase {
     }
 
     override void Persist() {
-        string data = MakeData();
-
-        FileHandle handle = OpenFile(Path(), FileMode.WRITE);
-        if (handle == 0) {
-            CarimLogging.Error(this, "Cannot open file for writing " + Path());
-            return;
-        }
-
-        FPrint(handle, data);
-        CloseFile(handle);
-
-        CarimLogging.Info(this, "Persisted at " + Path());
-    }
-
-    bool LoadData(string data) {
-        auto serializer = new JsonSerializer();
-
-        CarimLogging.Debug(this, "Deserializing from " + data);
-
         string error;
-        if (!serializer.ReadFromString(this, data, error)) {
-            CarimLogging.Error(this, "LoadData " + error);
-            return false;
+        if (!CarimModelAbcSerializers.SaveFile(this, error)) {
+            CarimLogging.Warn(this, "Error persisting at " + Path() + ", " + error);
+        } else {
+            CarimLogging.Info(this, "Persisted at " + Path());
         }
-        return true;
-    }
-
-    string MakeData() {
-        auto serializer = new JsonSerializer();
-
-        string output;
-        if (!serializer.WriteToString(this, true, output)) {
-            CarimLogging.Error(this, "MakeData failed");
-            return "";
-        }
-
-        CarimLogging.Debug(this, "Serialized to " + output);
-        return output;
     }
 }
 
